cb9b69211e17bbe31b252d2ac3abf942dfedf538
Abdallah Eldesouky <52884095+abdallah0x01@users.noreply.github.com>
2022-10-04 14:36:52 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,25 @@
 <?php
 
 include('conn.php');
+$isSuccess = 0;
 if (isset($_POST['login'])) {
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if (strlen($password) > 6) {
-        $query = "insert into users (username, password) values('$username','$password')";
-        $result = mysqli_query($conn, $query);
-        if ($result) {
-            header('location:index.php');
-        } else {
-            echo 'error';
-        }
-    } else {
-        echo 'password less than 6 chars';
-    }
-    // $query = "select * from users where username='$username' and password='$password'";
+    if (strlen($password) > 1) {
+        $query = "select username, password from users where username='$username' and password='$password'";
+        // "select username, password from users where username='' or 1=1"
 
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            // the condition below prevent sql injection technics like admin' -- 
+            if ($username == $row['username'] and $password == $row['password']) {
+                header('location:admin.php');
+                $isSuccess = 1;
+            }
+        }
+        if ($isSuccess == 0) {
+            header('location:404.php');
+        }
+    }
 }
